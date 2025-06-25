f0712ceb999eade8963087ff6a55df24c77178b9
Dibyanshu44 <dibyanshumohanty102@gmail.com>
2025-06-26 03:21:01 +0530

    chat images added

--- db.js
+++ db.js
@@ -1,26 +1,26 @@
-import pkg from "pg";
-const { Pool }=pkg;
+// import pkg from "pg";
+// const { Pool }=pkg;
 
-const pool=new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "mizu",
-    password: "jimmy123",
-    port: 5432,
-});
-
-export default pool;
-
-// db.js
-
-// import pkg from 'pg';
-// const { Pool } = pkg;
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
+// const pool=new Pool({
+//     user: "postgres",
+//     host: "localhost",
+//     database: "mizu",
+//     password: "jimmy123",
+//     port: 5432,
 // });
 
 // export default pool;
+
+// db.js
+
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export default pool;
